2205498dd8ac7964bb711c7bbe3e5e24e2a75ed7
wamisho-d <166142918+wamisho-d@users.noreply.github.com>
2025-02-26 23:50:31 -0500

    Update ProductConfirmationModal.jsx

--- my-react-app/src/components/Product/ProductConfirmationModal.jsx
+++ my-react-app/src/components/Product/ProductConfirmationModal.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Modal, Button } from 'react-bootstrap';
 
-const ProductConfirmationModal = ({ show, handleClose, handleConfirm, message }) => {
+const ProductConfirmationModal = ({ show, handleClose, handleConfirm, actionType }) => {
+  const actionText = actionType.charAt(0).toUpperCase() + actionType.slice(1);
+
   return (
     <Modal show={show} onHide={handleClose}>
       <Modal.Header closeButton>
-        <Modal.Title>Confirmation</Modal.Title>
+        <Modal.Title>Confirm {actionText}</Modal.Title>
       </Modal.Header>
-      <Modal.Body>{message}</Modal.Body>
+      <Modal.Body>
+        Are you sure you want to {actionType} this product?
+      </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={handleClose}>
           Cancel
         </Button>
         <Button variant="primary" onClick={handleConfirm}>
-          Confirm
+          {actionText}
         </Button>
       </Modal.Footer>
     </Modal>
