f64a2e621f4b7020282f173cc0fbffbca2112c98
wamisho-d <166142918+wamisho-d@users.noreply.github.com>
2025-02-27 00:11:06 -0500

    Create routes.jsx

---
+++ routes.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import CreateCustomerForm from './components/Customer/CreateCustomerForm';
+import CustomerDetails from './components/Customer/CustomerDetails';
+import UpdateCustomerForm from './components/Customer/UpdateCustomerForm';
+import DeleteCustomer from './components/Customer/DeleteCustomer';
+import CreateProductForm from './components/Product/CreateProductForm';
+import ProductDetails from './components/Product/ProductDetails';
+import UpdateProductForm from './components/Product/UpdateProductForm';
+import DeleteProduct from './components/Product/DeleteProduct';
+import ProductList from './components/Product/ProductList';
+import ProductStock from './components/Product/ProductStock';
+import PlaceOrderForm from './components/Order/PlaceOrderForm';
+import OrderHistory from './components/Order/OrderHistory';
+import CancelOrder from './components/Order/CancelOrder';
+import OrderTotal from './components/Order/OrderTotal';
+import ProductConfirmationModal from './components/Product/ProductConfirmationModal';
+
+const Routes = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/customers/create" component={CreateCustomerForm} />
+        <Route path="/customers/:id" component={CustomerDetails} />
+        <Route path="/customers/update/:id" component={UpdateCustomerForm} />
+        <Route path="/customers/delete/:id" component={DeleteCustomer} />
+        <Route path="/products/create" component={CreateProductForm} />
+        <Route path="/products/:id" component={ProductDetails} />
+        <Route path="/products/update/:id" component={UpdateProductForm} />
+        <Route path="/products/delete/:id" component={DeleteProduct} />
+        <Route path="/products" component={ProductList} />
+        <Route path="/products/stock" component={ProductStock} />
+        <Route path="/orders/create" component={PlaceOrderForm} />
+        <Route path="/orders/history" component={OrderHistory} />
+        <Route path="/orders/cancel/:id" component={CancelOrder} />
+        <Route path="/orders/total/:orderId" component={OrderTotal} />
+        <Route path="/products/confirmation" component={ProductConfirmationModal} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default Routes;
